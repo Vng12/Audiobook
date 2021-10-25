30ef80a95cfde15f7fc42d58457543a441cf7cb3
Vng12 <vedang.nilesh.gore@gmail.com>
2021-10-25 09:59:59 +0530

    Minor Change

--- Audiobook.py
+++ Audiobook.py
@@ -28,13 +28,13 @@ def img_to_text(_file):
     # converting image to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     fig = plt.figure(figsize=[10, 10])
-    # while plotting grayscale image with matplotlib, cmap should be defined
+
     plt.imshow(gray, cmap='gray', vmin=0, vmax=255)
     plt.show()
     text1 = pytesseract.image_to_string(gray)
     print(text1)
 
-    # lets try adaptive thresholding
+    # adaptive thresholding
     adaptive_threshold = cv2.adaptiveThreshold(
         gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
     fig = plt.figure(figsize=[10, 10])
@@ -47,7 +47,7 @@ def img_to_text(_file):
     print(type(img))
     print(height, width, channel)
 
-    # the output of OCR can be saved in a file in necessary
+    # the output of OCR can be saved in a file
     file = open('output.txt', 'a')  # file opened in append mode
     file.write(text2)
     file.close()
@@ -68,10 +68,6 @@ if _file.endswith(".pdf"):
         # Declaring filename for each page of PDF as JPG
         # For each page, filename will be:
         # PDF page 1 -> page_1.jpg
-        # PDF page 2 -> page_2.jpg
-        # PDF page 3 -> page_3.jpg
-        # ....
-        # PDF page n -> page_n.jpg
         filename = "page_"+str(image_counter)+".jpg"
 
         # Save the image of the page in system
@@ -81,11 +77,6 @@ if _file.endswith(".pdf"):
         image_counter = image_counter + 1
 
     filelimit = image_counter-1  # actual number of pages
-
-    # Creating a text file to write the output
-    outfile = "out_text.txt"
-
-    f = open(outfile, "a")
 
     for i in range(1, filelimit + 1):
 
